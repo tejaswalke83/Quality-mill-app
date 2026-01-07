33917cf7dfbccb9280ae02de386f17247cf79fde
tejaswalke83 <tejas.walke@gmail.com>
2026-01-07 23:30:08 +0530

    Update Supabase configuration in config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-window._env_ = {
-  SUPABASE_URL: "%%SUPABASE_URL%%",
-  SUPABASE_ANON_KEY: "%%SUPABASE_ANON_KEY%%"
-};
-
 // window._env_ = {
-//   SUPABASE_URL: "https://odczzymxgjsjhbufffxm.supabase.co",
-//   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kY3p6eW14Z2pzamhidWZmZnhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzMjY2NDIsImV4cCI6MjA2OTkwMjY0Mn0.T2ZT6U11taDH7vNTUJgI9xvqEZ1YEfBrNx28QV7jJvI"
+//   SUPABASE_URL: "%%SUPABASE_URL%%",
+//   SUPABASE_ANON_KEY: "%%SUPABASE_ANON_KEY%%"
 // };
+
+window._env_ = {
+  SUPABASE_URL: "https://odczzymxgjsjhbufffxm.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_SOdcBCszCGwvvi1rz7vA6w_6ZrKleJd"
+};
