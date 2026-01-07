85dcbd0e588526e3eb126fa22a5e4cf4e95320a3
tejaswalke83 <tejas.walke@gmail.com>
2026-01-07 23:50:01 +0530

    Update Supabase configuration to use placeholders
    Replaced hardcoded Supabase credentials with placeholders.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// window._env_ = {
-//   SUPABASE_URL: "%%SUPABASE_URL%%",
-//   SUPABASE_ANON_KEY: "%%SUPABASE_ANON_KEY%%"
-// };
-
 window._env_ = {
-  SUPABASE_URL: "https://odczzymxgjsjhbufffxm.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_SOdcBCszCGwvvi1rz7vA6w_6ZrKleJd"
+  SUPABASE_URL: "%%SUPABASE_URL%%",
+  SUPABASE_ANON_KEY: "%%SUPABASE_ANON_KEY%%"
 };
+
+// window._env_ = {
+//   SUPABASE_URL: "https://odczzymxgjsjhbufffxm.supabase.co",
+//   SUPABASE_ANON_KEY: "sb_publishable_SOdcBCszCGwvvi1rz7vA6w_6ZrKleJd"
+// };
